61534bae5f740bc688dd45892e582fd60a005192
Antti Rae <antti.rae@edu.turkuamk.fi>
2019-12-27 10:27:17 +0200

    simplify settings.ini, script now works on images

--- lookForMemes.py
+++ lookForMemes.py
@@ -8,10 +8,12 @@ config = configparser.ConfigParser()
 config.read('settings.ini')
 
 downloads_location = config['DEFAULT']['DownloadsLocation'] + '/'
-mp4_location = config['DEFAULT']['MP4Location']
-webm_location = config['DEFAULT']['WebmLocation']
-mp3_location = config['DEFAULT']['MP3Location']
-m4a_location = config['DEFAULT']['M4ALocation']
+mp4_location = config['VIDEOS']['MP4Location']
+webm_location = config['VIDEOS']['WebmLocation']
+mp3_location = config['SOUND']['MP3Location']
+m4a_location = config['SOUND']['M4ALocation']
+image_location = config['IMAGES']['Image']
+GIF_location = config['IMAGES']['GIF']
 
 # video files
 mp4 = compile('^.*\.mp4')
@@ -45,7 +47,9 @@ for meme in list:
     if(m4a.match(meme)):
         memes['m4a'].append(meme)
     if(image.match(meme)):
-        memes.['image'].append(meme)
+        memes['image'].append(meme)
+    if(gif.match(meme)):
+        memes['gif'].append(meme)
 
 print('Total number of found memes', len(memes))
 print('''Memes found:
@@ -53,10 +57,14 @@ print('''Memes found:
     {2} mp4 memes {3}
     {4} webm memes {5}
     {6} m4a memes {7}
+    {8} image memes {9}
+    {10} gif memes {11}
     '''.format(len(memes['mp3']), memes['mp3'],
     len(memes['mp4']), memes['mp4'],
     len(memes['webm']), memes['webm'],
-    len(memes['m4a']), memes['m4a']))
+    len(memes['m4a']), memes['m4a'],
+    len(memes['image']), memes['image'],
+    len(memes['gif']), memes['gif']))
 input('Checkpoint! [Enter] to copy found files, [CTRL + C] to cancel')
 
 for meme in memes['mp4']:
@@ -71,4 +79,10 @@ for meme in memes['mp3']:
 for meme in memes['m4a']:
     copy2(downloads_location + meme, m4a_location)
     print(meme, 'copied to', m4a_location)
+for meme in memes['image']:
+    copy2(downloads_location + meme, image_location)
+    print(meme, 'copied to', image_location)
+for meme in memes['gif']:
+    copy2(downloads_location + meme, GIF_location)
+    print(meme, 'copied to', GIF_location)
 print('Done')
